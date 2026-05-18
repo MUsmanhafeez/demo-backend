3302878d5786c2cd24ee61fb512471ba96725e85
MUsmanhafeez <usmanhafeez1478@gmail.com>
2026-05-18 17:42:53 +0500

    add console log on server file

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,10 @@ app.get("/health", (_req, res) => {
   res.json({ ok: true, service: "demo-backend" });
 });
 console.log("this is my backend")
+console.log("this is my backend")
+console.log("this is my backend")
+console.log("this is my backend")
+console.log("this is my backend")
 app.use("/api/items", itemsRouter);
 
 app.use((err: unknown, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
